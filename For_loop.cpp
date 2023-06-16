622c00439a354f0196deca027071ff10aa9732a9
Mansichanal663 <134155098+Mansichanal663@users.noreply.github.com>
2023-06-16 22:40:37 +0530

    Add files via upload

---
+++ For_loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+/*Basic for loop condition
+   for(int i = 0; i < 10; i++)
+   {
+   cout<<i<<endl;
+   }*/
+   for(int i = 1; i <= 100; i++)
+   {
+   cout<<i<<endl;
+   }
+    return 0;
+}
