c1df4690cab1f505b133ef2bffee185a66d9e6a6
Mansichanal663 <134155098+Mansichanal663@users.noreply.github.com>
2023-06-28 13:31:03 +0530

    Add files via upload

---
+++ C++/C language/enums.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    enum meal{morning, afternoon, evening, night};
+    cout<<morning;
+    cout<<afternoon;
+    cout<<evening;
+    cout<<night;
+    return 0;
+}
