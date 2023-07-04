3a12ef3d94fd1717df0871612b530a3fa1772976
Mansichanal663 <134155098+Mansichanal663@users.noreply.github.com>
2023-07-04 12:42:50 +0530

    Add files via upload

---
+++ C++/C language/reference+in+c++.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+// This will not swap a and b'''  
+int sum(int a, int b){
+       int c = a+b;
+       return c;
+}
+// // call by reference using pointer''''''
+// //   void swapPointer(int* a, int* b){
+// //          int temp = *a;
+// //          *a = *b;
+// //          *b = temp; 
+// } 
+
+// // call by reference using C++ reference variable ''''''
+  void swapReferenceVar(int &a, int &b){
+         int temp = a;
+          a = b;
+          b = temp;
+       }
+
+
+int main(){
+    int p=4, q=6;
+    // cout<<"The value of 4 and 6 is "<<sum(a,b);
+    cout<<"The value of p is "<<p<<"The value of q is "<<q<<endl;
+    // swappointer(&p, &q); // swap a and b using pointer reference''' 
+    //   swapPointer(&p, &q);
+    
+   // swapReference(p, q); // swap a and b using reference variable'''
+      swapReferenceVar(p, q);
+    cout<<"The value of p is "<<p<<"The value of q is "<<q<<endl;
+
+    return 0;
+}
